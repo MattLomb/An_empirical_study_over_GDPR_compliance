ba29e41c9625b73c4932f0d21c4892d48107cfc8
matteolombardi <matlomb97@gmail.com>
2023-10-25 18:15:59 +0200

    CMP Detector - from 6200 to 6400

--- cookiesFormatter.js
+++ cookiesFormatter.js
@@ -4,7 +4,8 @@ const path = require('path');
 // Funzione per leggere un file riga per riga e restituire le righe come un array
 async function formatCookies( url, cookies ) {
     return new Promise((resolve, reject) => {
-      const fileName = url.replace(/[^a-zA-Z0-9]/g, '_') + '.json';
+      //const fileName = url.replace(/[^a-zA-Z0-9]/g, '_') + '.json';
+      const fileName = url + '.json';
       const filePath = path.join(__dirname, fileName);
 
       // Scrivi i cookies nel file
